4f2bfd1254d2ff0a1350219e86a5b7fd0287a2e4
Anatoli4441 <160398483+Anatoli4441@users.noreply.github.com>
2024-08-10 17:35:17 +0200

    Update test_meme_api.py

--- tests/test_meme_api.py
+++ tests/test_meme_api.py
@@ -1,28 +1,27 @@
-from endpoints.endpoint import Endpoint
-
-
 class TestMemeAPI:
 
     def test_authorization(self, authorization):
-        response = authorization.authorize("Anatoliy")
-        Endpoint.check_status_code(response, 200)
+        authorization.authorize("Anatoliy")
+        authorization.check_status_code(200)
 
     def test_valid_token(self, do_get, token):
-        response = do_get.check_token(token)
-        Endpoint.check_status_code(response, 200)
+        do_get.check_token(token)
+        do_get.check_status_code(200)
 
     def test_get_all_memes(self, do_get):
-        response = do_get.get_all_memes()
-        Endpoint.check_status_code(response, 200)
+        do_get.get_all_memes()
+        do_get.check_status_code(200)
 
     def test_get_meme_by_id(self, meme, do_get, meme_data):
         meme_id = meme["id"]
-        response = do_get.get_meme_by_id(meme_id)
-        Endpoint.check_status_code(response, 200)
-        Endpoint.check_meme_data(response.json(), meme_data)
+        do_get.get_meme_by_id(meme_id)
+        do_get.check_status_code(200)
+        do_get.check_meme_data(meme_data)
 
-    def test_add_meme(self, meme, meme_data):
-        Endpoint.check_meme_data(meme, meme_data)
+    def test_add_meme(self, do_post, meme_data):
+        do_post.add_meme(meme_data)
+        do_post.check_status_code(200)
+        do_post.check_meme_data(meme_data)
 
     def test_update_meme(self, meme, do_put, do_get):
         meme_id = meme["id"]
@@ -33,29 +32,29 @@ class TestMemeAPI:
             "tags": ["black", "cat", "green eyes"],
             "info": {"colours": ["black", "red", "green"]}
         }
-        response = do_put.update_meme(meme_id, updated_data)
-        Endpoint.check_status_code(response, 200)
+        do_put.update_meme(meme_id, updated_data)
+        do_put.check_status_code(200)
 
-        response = do_get.get_meme_by_id(meme_id)
-        Endpoint.check_status_code(response, 200)
-        Endpoint.check_meme_data(response.json(), updated_data)
+        do_get.get_meme_by_id(meme_id)
+        do_get.check_status_code(200)
+        do_get.check_meme_data(updated_data)
 
     def test_delete_meme(self, meme, do_delete, do_get):
         meme_id = meme["id"]
 
-        response = do_delete.delete_meme(meme_id)
-        Endpoint.check_status_code(response, 200)
+        do_delete.delete_meme(meme_id)
+        do_delete.check_status_code(200)
 
-        response = do_get.get_meme_by_id(meme_id)
-        Endpoint.check_status_code(response, 404)
+        do_get.get_meme_by_id(meme_id)
+        do_get.check_status_code(404)
 
     def test_invalid_token(self, do_get):
-        response = do_get.check_token("xsdsd232333")
-        Endpoint.check_status_code(response, 404)
+        do_get.check_token("xsdsd232333")
+        do_get.check_status_code(404)
 
     def test_nonexistent_meme(self, do_get):
-        response = do_get.get_meme_by_id("3234348949493x")
-        Endpoint.check_status_code(response, 404)
+        do_get.get_meme_by_id("3234348949493x")
+        do_get.check_status_code(404)
 
     def test_add_meme_without_url(self, do_post):
         data = {
@@ -63,8 +62,8 @@ class TestMemeAPI:
             "tags": ["black", "cat"],
             "info": {"colours": ["black", "red"]}
         }
-        response = do_post.add_meme(data)
-        Endpoint.check_status_code(response, 400)
+        do_post.add_meme(data)
+        do_post.check_status_code(400)
 
     def test_update_meme_with_nonexistent_id(self, do_put):
         data = {
@@ -74,13 +73,13 @@ class TestMemeAPI:
             "tags": ["black", "cat", "green eyes"],
             "info": {"colours": ["black", "red", "green"]}
         }
-        response = do_put.update_meme("3234348949493x", data)
-        Endpoint.check_status_code(response, 404)
+        do_put.update_meme("3234348949493x", data)
+        do_put.check_status_code(404)
 
     def test_delete_meme_created_by_other_user(self, do_delete, other_user_meme):
         meme_id = other_user_meme["id"]
-        response = do_delete.delete_meme(meme_id)
-        Endpoint.check_status_code(response, 403)
+        do_delete.delete_meme(meme_id)
+        do_delete.check_status_code(403)
 
     def test_add_meme_with_int_in_text(self, do_post):
         data = {
@@ -89,8 +88,8 @@ class TestMemeAPI:
             "tags": ["black", "cat"],
             "info": {"colours": ["black", "red"]}
         }
-        response = do_post.add_meme(data)
-        Endpoint.check_status_code(response, 400)
+        do_post.add_meme(data)
+        do_post.check_status_code(400)
 
     def test_add_meme_with_int_in_url(self, do_post):
         data = {
@@ -99,12 +98,12 @@ class TestMemeAPI:
             "tags": ["black", "cat"],
             "info": {"colours": ["black", "red"]}
         }
-        response = do_post.add_meme(data)
-        Endpoint.check_status_code(response, 400)
+        do_post.add_meme(data)
+        do_post.check_status_code(400)
 
     def test_unauthorized_access_get(self, unauthorized_do_get):
-        response = unauthorized_do_get.get_all_memes()
-        Endpoint.check_status_code(response, 401)
+        unauthorized_do_get.get_all_memes()
+        unauthorized_do_get.check_status_code(401)
 
     def test_unauthorized_access_post(self, unauthorized_do_post):
         data = {
@@ -113,8 +112,8 @@ class TestMemeAPI:
             "tags": ["black", "cat"],
             "info": {"colours": ["black", "red"]}
         }
-        response = unauthorized_do_post.add_meme(data)
-        Endpoint.check_status_code(response, 401)
+        unauthorized_do_post.add_meme(data)
+        unauthorized_do_post.check_status_code(401)
 
     def test_unauthorized_access_put(self, unauthorized_do_put, anatoliy_memes):
         meme_to_update = next((m for m in anatoliy_memes), None)
@@ -127,12 +126,12 @@ class TestMemeAPI:
             "tags": ["black", "cat", "green eyes"],
             "info": {"colours": ["black", "red", "green"]}
         }
-        response = unauthorized_do_put.update_meme(meme_to_update["id"], updated_data)
-        Endpoint.check_status_code(response, 401)
+        unauthorized_do_put.update_meme(meme_to_update["id"], updated_data)
+        unauthorized_do_put.check_status_code(401)
 
     def test_unauthorized_access_delete(self, unauthorized_do_delete, anatoliy_memes):
         meme_to_delete = next((m for m in anatoliy_memes), None)
         assert meme_to_delete is not None, "No meme found created by Anatoliy"
 
-        response = unauthorized_do_delete.delete_meme(meme_to_delete["id"])
-        Endpoint.check_status_code(response, 401)
+        unauthorized_do_delete.delete_meme(meme_to_delete["id"])
+        unauthorized_do_delete.check_status_code(401)
